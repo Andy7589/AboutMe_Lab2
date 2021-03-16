e27d7f38ed801ea027d4d4702307d175cd01b2dd
Andres Torreschavez <A.torreschavez@hotmail.com>
2021-03-16 03:12:58 -0700

    new structured update

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+let response1 = prompt("Whats the Password?");
+alert(response1);
+console.log('my response to question 1 was...' + response1);
+alert('Eh, close enough!');
+
+let response2 = prompt("who's your daddy and what does he do?");
+alert(response2);
+console.log('my response to question 2 was...' + response2);
+alert('makes sense...');
+
+let response3 = prompt('sooo how you doin?');
+alert(response3);
+console.log('my response to queation 3 was...' + response3);
+alert('NOICE!')
+
+let response4 = prompt('Ready to enter my page?');
+alert(response4);
+console.log('my response to question 4 was...' + response4);
+alert('no you arent')
